51cd52c310d730571de516a9aced25e7ed440d42
DaianF <106494989+DaianF@users.noreply.github.com>
2023-04-07 18:08:04 -0300

    Add files via upload

---
+++ Ejer1.js
@@ -0,0 +1,10 @@
+const yo=["Daian",25,true];
+
+const fecha = new Date ("february 10 98");
+
+const libro = {
+    titulo: "Personas toxicas",
+    autor: "Julia Máxima Uriarte",
+    fecha: "26 de junio de 2019",
+    url: "https://www.amazon.com/-/es/Dr-Lillian-Glass/dp/1929873441/ref=sr_1_2?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=toxic+people&sr=8-2"
+}
